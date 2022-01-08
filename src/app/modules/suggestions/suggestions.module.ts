09973f8333f7d724e4a672d2fcdeb0aad00db468
Godm0de <Godm0de@github.io>
2022-01-08 15:38:00 +0100

    Create feedback component

--- src/app/modules/suggestions/suggestions.module.ts
+++ src/app/modules/suggestions/suggestions.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { SuggestionsLayoutComponent } from './layout/suggestions-layout.component';
+import { FeedbackCardComponent } from './components/feedback-card/feedback-card.component';
+import { UICounterModule } from '@shared/ui-components/counter/ui-counter.module';
+import { UIBadgeModule } from '@shared/ui-components/badge/ui-badge.module';
+import { UIChipsModule } from '@shared/ui-components/chips/ui-chips.module';
+import { UIIconModule } from '@shared/ui-components/icon/ui-icon.module';
 
 const routes: Routes = [
     {
@@ -12,8 +17,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    declarations: [SuggestionsLayoutComponent],
-    imports: [CommonModule, RouterModule.forChild(routes)],
+    declarations: [SuggestionsLayoutComponent, FeedbackCardComponent],
+    imports: [CommonModule, RouterModule.forChild(routes), UICounterModule, UIBadgeModule, UIChipsModule, UIIconModule],
     exports: [RouterModule],
 })
 export class SuggestionsModule {}
