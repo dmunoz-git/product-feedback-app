c220e167a39b1bbf824b90fdcfde8437001aeb8e
Godm0de <Godm0de@github.io>
2022-01-22 13:46:14 +0100

    Addapt banner to responsive

--- src/app/modules/suggestions/suggestions.module.ts
+++ src/app/modules/suggestions/suggestions.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TranslocoModule } from '@ngneat/transloco';
 import { UIBadgeModule } from '@shared/ui-components/badge/ui-badge.module';
 import { UIChipsModule } from '@shared/ui-components/chips/ui-chips.module';
 import { UICounterModule } from '@shared/ui-components/counter/ui-counter.module';
 import { UIIconModule } from '@shared/ui-components/icon/ui-icon.module';
+import { BannerComponent } from './components/banner/banner.component';
 import { FeedbackCardModule } from './components/feedback-card/feedback-card.module';
 import { SuggestionsLayoutComponent } from './layout/suggestions-layout.component';
 
@@ -17,8 +19,17 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    declarations: [SuggestionsLayoutComponent],
-    imports: [CommonModule, RouterModule.forChild(routes), UICounterModule, UIBadgeModule, UIChipsModule, UIIconModule, FeedbackCardModule],
+    declarations: [SuggestionsLayoutComponent, BannerComponent],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        UICounterModule,
+        UIBadgeModule,
+        UIChipsModule,
+        UIIconModule,
+        FeedbackCardModule,
+        TranslocoModule,
+    ],
     exports: [RouterModule],
 })
 export class SuggestionsModule {}
