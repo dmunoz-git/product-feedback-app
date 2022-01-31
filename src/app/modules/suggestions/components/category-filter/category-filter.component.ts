54961b87a3a567e61df7e0c348313c383885e849
Godm0de <Godm0de@github.io>
2022-01-31 20:39:57 +0100

    Modify filter

--- src/app/modules/suggestions/components/category-filter/category-filter.component.ts
+++ src/app/modules/suggestions/components/category-filter/category-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-category-filter',
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
     styleUrls: ['./category-filter.component.scss'],
 })
 export class CategoryFilterComponent {
-    categories: string[] = ['all', 'ui', 'ux', 'enhancement', 'bug', 'feature'];
+    @Output() categorySelected = new EventEmitter<string>();
+
+    categories = [
+        {
+            name: 'all',
+            selected: true,
+        },
+        {
+            name: 'ui',
+            selected: false,
+        },
+        {
+            name: 'ux',
+            selected: false,
+        },
+        {
+            name: 'enhancement',
+            selected: false,
+        },
+        {
+            name: 'bug',
+            selected: false,
+        },
+        {
+            name: 'feature',
+            selected: false,
+        },
+    ];
+
+    setCategory(categoryName: string) {
+        this.categories.forEach((c) => (c.selected = c.name === categoryName));
+
+        this.categorySelected.emit(categoryName);
+    }
+
+    getCategoryActive(category: string) {
+        return this.categories.filter((c) => c.name === category)[0].selected;
+    }
 }
--- src/app/shared/ui-components/chips/chip/ui-chip.component.ts
+++ src/app/shared/ui-components/chips/chip/ui-chip.component.ts
@@ -3,13 +3,7 @@ import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 @Component({
     // eslint-disable-next-line @angular-eslint/component-selector
     selector: 'ui-chip',
-    template: ` <span
-        class="ui-chip"
-        (click)="setActiveState()"
-        [class.ui-chip--active]="active"
-        [class.ui-chip--selected]="selectable"
-        #text
-    >
+    template: ` <span class="ui-chip" [class.ui-chip--active]="active" [class.ui-chip--selected]="selectable" #text>
         <ng-content></ng-content>
     </span>`,
     styleUrls: ['./ui-chip.component.scss'],
@@ -17,11 +11,6 @@ import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 export class UIChipComponent {
     @ViewChild('text') textElement!: ElementRef;
     @Input() selectable: boolean = true;
-    @Input() value!: string;
-    @Input() key!: string;
-    active!: boolean;
-
-    setActiveState(): void {
-        this.active = true;
-    }
+    @Input() value: string = '';
+    @Input() active: boolean = false;
 }
