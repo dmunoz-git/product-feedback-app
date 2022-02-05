66ddddc125caaa42c88f3b114a561249757e951a
Godm0de <Godm0de@github.io>
2022-02-05 20:26:21 +0100

    Remove unnecesary toast

--- src/app/shared/ui-components/toast/ui-toast-data.ts
+++
@@ -1,8 +0,0 @@
-export interface UiToastData {
-    message: string;
-    duration?: number;
-    close?: string;
-    position?: UiToastPosition;
-}
-
-export type UiToastPosition = 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left' | 'top-center' | 'bottom-center' | 'center';
--- src/app/shared/ui-components/toast/ui-toast-tokens.ts
+++
@@ -1,4 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { UiToastData } from './ui-toast-data';
-
-export const UI_TOAST_DATA = new InjectionToken<UiToastData>('TOAST_DATA');
