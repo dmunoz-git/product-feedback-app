cd268b6afbb35b3f8343f132e3da3757d7cca475
Godm0de <Godm0de@github.io>
2022-02-06 20:57:32 +0100

    Remove container css class from layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: `<div class="container"><router-outlet></router-outlet></div>`,
+    template: `<router-outlet></router-outlet>`,
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
