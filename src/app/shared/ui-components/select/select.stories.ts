4d5841b334b7863a89461aa771bd537c80d3c1e9
Godm0de <Godm0de@github.io>
2022-02-06 20:05:28 +0100

    Add select stories

--- src/app/shared/ui-components/select/select.stories.ts
+++ src/app/shared/ui-components/select/select.stories.ts
@@ -1,33 +1,37 @@
-import { CommonModule } from '@angular/common';
 import { moduleMetadata } from '@storybook/angular';
 import { Story } from '@storybook/angular/types-6-0';
-import { UiInputDirective } from './ui-input.directive';
+import { UiSelectPanelComponent } from './select-panel/ui-select-panel.component';
+import { UiSelectModule } from './ui-select.module';
 
 // This exports the Stories group for this component
 export default {
     // The title defines the name and where in the structure of
     // Storybook's menu this is going to be placed.
     // Here we add it to a "Components" section under "Link"
-    title: 'Components/Input',
+    title: 'Components/Select',
     // The component related to the Stories
-    component: UiInputDirective,
+    component: UiSelectPanelComponent,
 
     decorators: [
         // The necessary modules for the component to work on Storybook
         moduleMetadata({
-            declarations: [UiInputDirective],
-            imports: [CommonModule],
+            imports: [UiSelectModule],
         }),
     ],
 };
 // This creates a Story for the component
-const Template: Story<UiInputDirective> = (args: UiInputDirective) => ({
+const Template: Story<UiSelectPanelComponent> = (args: UiSelectPanelComponent) => ({
     props: args,
     moduleMetadata: {
-        declarations: [UiInputDirective],
-        imports: [CommonModule],
+        imports: [UiSelectModule],
     },
-    template: `<input uiInput [ngModel]="value" placeholder="This is sparta!"/>`,
+    template: `
+	<ui-select label="Name" placeholder="Select the option">
+		<ui-select-option value="option1">Option 1</ui-select-option>
+		<ui-select-option value="option2">Option 2</ui-select-option>
+		<ui-select-option value="option3">Option 3</ui-select-option>
+	</ui-select>
+`,
 });
 
 export const Basic = Template.bind({});
