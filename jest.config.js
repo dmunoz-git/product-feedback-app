b6a7a0ddedd0eeddfa6839b71b9858b89eb7fee4
Godm0de <Godm0de@github.io>
2022-02-13 17:22:15 +0100

    Map the relative ts paths to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     moduleNameMapper: {
-        '@core/(.*)': '<rootDir>/src/app/core/$1',
+        '^@core/(.*)': '<rootDir>/src/app/core/$1',
+        '^@modules/(.*)$': '<rootDir>/src/app/modules/$1',
+        '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
+        '^@environments/(.*)$': '<rootDir>/src/environments/$1',
     },
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
