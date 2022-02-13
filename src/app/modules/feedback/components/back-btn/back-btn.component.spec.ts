450018c43fba9e507f3c05c0ec8aeceb7c288063
Godm0de <Godm0de@github.io>
2022-02-13 20:23:45 +0100

    Fix inject spec

--- src/app/modules/feedback/components/back-btn/back-btn.component.spec.ts
+++ src/app/modules/feedback/components/back-btn/back-btn.component.spec.ts
@@ -1,25 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { UiIconModule } from '@shared/ui-components/icon/ui-icon.module';
 import { BackBtnComponent } from './back-btn.component';
 
 describe('BackBtnComponent', () => {
-  let component: BackBtnComponent;
-  let fixture: ComponentFixture<BackBtnComponent>;
+    let component: BackBtnComponent;
+    let fixture: ComponentFixture<BackBtnComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BackBtnComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [BackBtnComponent],
+            imports: [UiIconModule],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BackBtnComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(BackBtnComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
