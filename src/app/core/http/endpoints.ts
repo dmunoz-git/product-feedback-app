d3be2d2e38f7297d4b882bea70d6e80464f492ba
Godm0de <Godm0de@github.io>
2022-02-13 17:24:07 +0100

    Remove unnecesary endpoints file

--- src/app/core/http/endpoints.ts
+++
@@ -1,8 +0,0 @@
-import { environment } from '@enviroments/environment';
-
-export const endpoints = {
-    users: `${environment.apiUrl}/users`,
-    comments: `${environment.apiUrl}/comments`,
-    feedbacks: `${environment.apiUrl}/feedbacks`,
-    replies: `${environment.apiUrl}/replies`,
-};
