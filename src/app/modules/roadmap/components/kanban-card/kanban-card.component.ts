2ce845c26e058aef4d84d9224057babe97bbf343
Godm0de <Godm0de@github.io>
2022-04-10 19:59:46 +0200

    Add custom colors to kanban card

--- src/app/modules/roadmap/components/kanban-card/kanban-card.component.ts
+++ src/app/modules/roadmap/components/kanban-card/kanban-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-kanban-card',
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./kanban-card.component.scss']
 })
 export class KanbanCardComponent  {
-
+  @Input() color: 'primary' | 'link' | 'tertiary' | 'none' = 'none';
 
 }
