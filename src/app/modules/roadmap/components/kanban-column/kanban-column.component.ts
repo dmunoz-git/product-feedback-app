021c06904e674cc1ce1082d54fb966671919934f
Godm0de <Godm0de@github.io>
2022-04-11 21:37:49 +0200

    Add props to kanban-column component

--- src/app/modules/roadmap/components/kanban-column/kanban-column.component.ts
+++ src/app/modules/roadmap/components/kanban-column/kanban-column.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-kanban-column',
   templateUrl: './kanban-column.component.html',
   styleUrls: ['./kanban-column.component.scss']
 })
-export class KanbanColumnComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class KanbanColumnComponent {
+  @Input() title:string = "Title";
+  @Input() subtitle: string = "subtitle";
 }
