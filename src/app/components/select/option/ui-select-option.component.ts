96feddef57b29bee4a678ee1f0e8e1ae637418c0
godm0de <godm0de@github.io>
2022-12-01 16:00:42 +0100

    fix: :bug: change disabled by data disable

--- src/app/components/select/option/ui-select-option.component.ts
+++ src/app/components/select/option/ui-select-option.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Host, HostListener, Input, Optional, ViewChild, ViewEncapsulation } from '@angular/core';
 import { UiSelectPanelComponent } from '../panel/ui-select-panel.component';
 
-
 export interface IUiSelectOption {
     value: string;
     text: string;
@@ -12,16 +11,16 @@ export interface IUiSelectOption {
     // eslint-disable-next-line @angular-eslint/component-selector
     selector: 'ui-select-option',
     template: `
-    <button class="ui-select-option" [class.ui-select-option--selected]="selected" #text>
-        <ng-content></ng-content><img *ngIf="selected" src="assets/images/icons/icon-check.svg" alt="" />
-    </button> `,
+        <button class="ui-select-option" [class.ui-select-option--selected]="data.selected" #text>
+            <ng-content></ng-content><img *ngIf="data.selected" src="assets/images/icons/icon-check.svg" alt="" />
+        </button>
+    `,
     styleUrls: ['./ui-select-option.component.scss'],
     encapsulation: ViewEncapsulation.None,
 })
 export class UiSelectOptionComponent implements AfterViewInit {
     @Input() value!: string;
     @ViewChild('text') displayText!: ElementRef;
-    selected: boolean = false;
 
     public data: IUiSelectOption = {
         value: '',
@@ -31,10 +30,9 @@ export class UiSelectOptionComponent implements AfterViewInit {
 
     constructor(@Optional() @Host() private panel: UiSelectPanelComponent) {}
 
-    ngAfterViewInit() { 
+    ngAfterViewInit() {
         this.data.text = this.displayText.nativeElement.innerText;
         this.data.value = this.value;
-        this.selected = this.data.selected;
     }
 
     @HostListener('click', ['$event'])
